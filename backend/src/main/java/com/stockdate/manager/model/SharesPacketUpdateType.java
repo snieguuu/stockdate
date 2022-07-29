1c8b9b71ff2a1fd5aed56cb3ee83dac1b0740f75
snieguuu <sniegu@gmail.com>
2022-07-29 14:15:47 +0200

    New enum for representing update types of shares packets.

---
+++ backend/src/main/java/com/stockdate/manager/model/SharesPacketUpdateType.java
@@ -0,0 +1,15 @@
+package com.stockdate.manager.model;
+
+/**
+ * Update type for {@link SharesPacket}
+ */
+public enum SharesPacketUpdateType {
+    /**
+     * SELL for selling shares from particular shares packet
+     */
+    SELL,
+    /**
+     * BUY for buying shares of particular shares packet
+     */
+    BUY
+}
