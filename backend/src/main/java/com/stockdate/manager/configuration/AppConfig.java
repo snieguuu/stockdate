2b3839aeb787cd51143c8dc2ed84e97ca60a22fb
snieguuu <sniegu@gmail.com>
2022-07-29 12:44:09 +0200

    Adding configuration class for future use.

---
+++ backend/src/main/java/com/stockdate/manager/configuration/AppConfig.java
@@ -0,0 +1,20 @@
+package com.stockdate.manager.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configuration class for Stockdate application.
+ */
+@Configuration
+public class AppConfig {
+
+//    @Value("${sum.intended.for.investing}")
+//    private double sumIntendedForInvesting;
+//
+//    @Bean("sumIntendedForInvesting")
+//    public double getSumIntendedForInvesting() {
+//        return sumIntendedForInvesting;
+//    }
+}
